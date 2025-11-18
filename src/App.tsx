c8aaa70b83c658020362b87379b8eb57002c1b38
Danayit Mulugeta <38809362+Dayni0603@users.noreply.github.com>
2025-11-18 15:13:56 +0300

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/footer";
 import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function footer() {
+  return (
+    <footer className="bg-gray-100 text-gray-700 py-4 text-center mt-auto">
+      <p>
+        &copy; {new Date().getFullYear()} Habit Tracker. All rights reserved.
+      </p>
+      <p>
+      </p>
+    </footer>
+  );
+}
+
+export default footer;
