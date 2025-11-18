0f18ee9ccd2bf88ae3d4ad8c8214d029c9b56394
Danayit Mulugeta <38809362+Dayni0603@users.noreply.github.com>
2025-11-18 15:19:49 +0300

    footer update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function footer() {
   return (
-    <footer className="bg-gray-100 text-gray-700 py-4 text-center mt-auto">
+    <footer>
       <p>
         &copy; {new Date().getFullYear()} Habit Tracker. All rights reserved.
       </p>
